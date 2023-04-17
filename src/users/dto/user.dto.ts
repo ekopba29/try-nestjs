7099514d66b51ec2af0c23bf4e6214fcc3848779
epba <ekopurnomo.office@gmail.com>
2023-04-17 19:46:40 +0700

    date validation

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,31 +1,34 @@
 import { Optional } from "@nestjs/common";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsAlpha, IsDate, IsEmail, IsNotEmpty, IsEnum, IsEmpty, IsDateString, ValidateIf } from "class-validator";
+import { Transform, Type } from "class-transformer";
+import { IsAlpha, IsDate, IsEmail, IsNotEmpty, IsEnum, IsEmpty, IsDateString, ValidateIf, MinDate } from "class-validator";
+import { GenderPipe } from "../custom-validation/gender.validation";
 
 export class UserDTO {
 
-    @ApiProperty({ description: 'username', example: 'epba'})
+    @ApiProperty({ description: 'username', example: 'epba' })
     @IsNotEmpty()
     username: String;
 
-    @ApiProperty({ description: 'name', example: 'Eko Purnomo'})
+    @ApiProperty({ description: 'name', example: 'Eko Purnomo' })
     @IsNotEmpty()
     name: String;
 
-    @ApiProperty({ description: 'username', example: 'ekopurnomo.office@gmail.com'})
+    @ApiProperty({ description: 'username', example: 'ekopurnomo.office@gmail.com' })
     @IsEmail()
     email: String;
 
-    @ApiProperty({ description: 'password', example: '@#FSOjfjsdoj'})
+    @ApiProperty({ description: 'password', example: '@#FSOjfjsdoj' })
     @IsNotEmpty()
     password: String;
 
-    @ApiProperty({ description: 'gender between MALE | FEMALE', example: 'Male'})
+    @ApiProperty({ description: 'gender between MALE | FEMALE', example: 'Male' })
     @IsNotEmpty()
-    // @ValidateIf((object, value) => ['Male', 'Female'].includes(value))
+    @ValidateIf((object, value) => !['Male', 'Female'].includes(value))
     gender: String;
 
-    // @IsDate()
+    @IsNotEmpty()
+    @IsDateString()
     birth_date: Date;
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,9 +22,9 @@ export class UsersService {
         if (updateData.birth_date) {
             const birthDate = new Date(updateData.birth_date);
             const [day, month, year]: [Number, Number, Number] = [birthDate.getDay(), birthDate.getMonth() + 1, birthDate.getFullYear()];
-            // const zodiac = getSign(year)
-            // const shio = getZodiac({ month:month, day:day })
-            // updateData = Object.assign({...updateData, zodiac, shio}) 
+            const zodiac = getSign({ month: month, day: day })
+            const shio = getZodiac(year)
+            updateData = Object.assign({ ...updateData, zodiac, shio })
         }
 
         return this.userRepository.findOneAndUpdate({ _id }, updateData);
@@ -41,12 +41,10 @@ export class UsersService {
         const about: String = '';
         const interests: [] = []
 
-        // const birthDate = new Date(userDto.birth_date);
-        // const [day, month, year]: [Number, Number, Number] = [birthDate.getDay(), birthDate.getMonth()+1, birthDate.getFullYear()];
-        // const zodiac = getSign(year)
-        // const shio = getZodiac({ month, day })
-        const zodiac = ''
-        const shio = ''
+        const birthDate = new Date(userDto.birth_date);
+        const [day, month, year]: [Number, Number, Number] = [birthDate.getDay(), birthDate.getMonth() + 1, birthDate.getFullYear()];
+        const zodiac = getSign({ month, day })
+        const shio = getZodiac(year)
 
         const paramUser: User = {
             ...userDto,
